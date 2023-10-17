f96c536bc2bd304bdbf42151e364ad807b4bfb80
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-10-17 18:59:26 +0200

    flex

--- src/components/ui/controlled/ControlledTextField.tsx
+++ src/components/ui/controlled/ControlledTextField.tsx
@@ -1,8 +1,10 @@
+import { useController, FieldValues, UseControllerProps } from 'react-hook-form'
 
-import {FieldValue, FieldValues} from "react-hook-form";
+import { TextFieldProps } from '../textField'
 
-export type ControlledTextFieldProps<T extends FieldValues> =
+export type ControlledTextFieldProps<T extends FieldValues> = UseControllerProps<T> &
+  Omit<TextFieldProps, 'id' | 'onChange' | 'value'>
 
-export const ControlledTextField = () => {
+export const ControlledTextField = <T extends FieldValues>({}: ControlledTextFieldProps<T>) => {
   return <div></div>
 }
