52e02c18e5e9a64578628891e4da7f210f266725
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-11-11 16:43:49 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css'
+import './App.module.scss'
 import { Header } from './components/header/Header.tsx'
 
 export function App() {
-
-
-
 
   return (
     <div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,15 @@
 
+import c from '../../App.module.scss'
+
 import s from './header.module.scss'
 
 export const Header = () => {
   return (
-    <div className={s.header}></div>
+    <header className={s.header}>
+      <div className={c.container}>
+        <div className={s.headerInner}></div>
+      </div>
+    </header>
   )
 
 };
