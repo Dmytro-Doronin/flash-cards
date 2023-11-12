334b09fc52513abb02094c8687b97806073cb14e
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-11-12 10:22:05 +0100

    profile menu

--- src/components/profileContent/ProfileContent.tsx
+++ src/components/profileContent/ProfileContent.tsx
@@ -1,20 +1,21 @@
-import { FC } from 'react'
+import { useState } from 'react'
 
 import avatar from '../../assets/images/IMG_0390.jpg'
-import { ProfileAvatar, ProfileAvatarType } from '../profileAvatar/ProfileAvatar.tsx'
+import { ProfileAvatar } from '../profileAvatar/ProfileAvatar.tsx'
 import { ProfileMenu } from '../profileMenu/ProfileMenu.tsx'
 
 import c from './profileContent.module.scss'
 
-export const ProfileContent: FC<ProfileAvatarType> = () => {
+export const ProfileContent = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
   const sayHi = () => {
-    console.log('hi')
+    setIsVisible(isVisible => !isVisible)
   }
 
   return (
     <div className={c.profileContent}>
       <ProfileAvatar image={avatar} callback={sayHi} />
-      <ProfileMenu />
+      <ProfileMenu isVisible={isVisible} />
     </div>
   )
 }
--- src/components/profileMenu/ProfileMenu.tsx
+++ src/components/profileMenu/ProfileMenu.tsx
@@ -1,9 +1,41 @@
+import { FC } from 'react'
+
+import { clsx } from 'clsx'
+
+import avatar from '../../assets/images/IMG_0390.jpg'
+import { ProfileAvatar } from '../profileAvatar/ProfileAvatar.tsx'
+import { Typography } from '../ui/typography'
+
 import c from './profileMenu.module.scss'
 
-export const ProfileMenu = () => {
+type ProfileMenuType = {
+  isVisible: boolean
+  isInformation: boolean
+}
+
+export const ProfileMenu: FC<ProfileMenuType> = ({ isVisible, isInformation }) => {
+  const styles = isVisible ? clsx(c.profileMenu, c.visible) : c.profileMenu
+  const name = 'Dima'
+  const email = 'asdffa@asd.com'
+
+  const
+
   return (
-    <div className={c.profileMenu}>
+    <div className={styles}>
       <div className={c.triangleMenu}></div>
+      <div className={c.infoBlock}>
+        <ProfileAvatar image={avatar} />
+        <div className={c.textBlock}>
+          <Typography variant={'body1'}>{name}</Typography>
+          <Typography className={c.email} variant={'caption'}>
+            {email}
+          </Typography>
+        </div>
+      </div>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
