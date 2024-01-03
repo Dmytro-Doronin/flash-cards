5e9e22d32dc4e0472a4597d1d6c0d07039a291ce
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-01-03 22:27:39 +0100

    ErrorSnackbar

---
+++ src/components/errorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+import { CheckCircledIcon, CrossCircledIcon } from '@radix-ui/react-icons'
+import { clsx } from 'clsx'
+
+import c from './errorSnackbar.module.scss'
+
+type ErrorSnackbarType = {
+  variant: 'success' | 'error'
+  error: any
+}
+
+export const ErrorSnackbar = ({ variant, error }: ErrorSnackbarType) => {
+  const [isOpen, setIsOpen] = useState(!!error)
+  const classes = {
+    toastBlock: clsx(isOpen ? `${c.toastBox} ${c.isOpen}` : c.toastBox),
+    iconBlock: clsx(c.iconBlock, c[variant]),
+  }
+
+  useEffect(() => {
+    setIsOpen(error && true)
+    const timeout = setTimeout(() => {
+      setIsOpen(false)
+    }, 4000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  return (
+    <div className={classes.toastBlock}>
+      <div className={classes.iconBlock}>
+        {variant === 'success' ? (
+          <CheckCircledIcon width="40" height="40" />
+        ) : (
+          <CrossCircledIcon width="40" height="40" />
+        )}
+      </div>
+      <div className={c.messageBlock}>{error.data?.message}</div>
+    </div>
+  )
+}
--- src/pages/signInPage/SignInPage.tsx
+++ src/pages/signInPage/SignInPage.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { LoginForm } from '../../components/auth/loginForm/LoginForm.tsx'
 // import RecoverPassword from '../../components/auth/recoverPassword/RecoverPassword.tsx'
 // import { CheckEmail } from '../../components/auth/checkEmail/CheckEmail.tsx'
+import { ErrorSnackbar } from '../../components/errorSnackbar/ErrorSnackbar.tsx'
 import { Header } from '../../components/header/Header.tsx'
 import { useLoginMutation } from '../../services/auth/auth.service.ts'
 import { LoginType } from '../../services/auth/auth.types.ts'
@@ -11,21 +12,27 @@ import { LoginType } from '../../services/auth/auth.types.ts'
 import c from './signIn.module.scss'
 
 export const SignInPage = () => {
-  const [login, { isLoading,error }] = useLoginMutation()
+  const [login, { isLoading, error, isError }] = useLoginMutation()
   const navigate = useNavigate()
+
+
+  
   const handleSubmit = async (args: LoginType) => {
-    try {
-      await login(args)
+    const result = await login(args)
+
+    if (!result.error) {
       navigate('/')
-    } catch (e) {
-      console.log(e)
     }
   }
-  console.log(error)
+
+  // console.log(error)
+  // console.log(error)
+  
   return (
     <div className={c.signIn}>
       <Header variant="signUp" />
       <LoginForm isLoading={isLoading} onSubmit={handleSubmit} />
+      {error && <ErrorSnackbar error={error} variant="error" />}
     </div>
   )
 }
